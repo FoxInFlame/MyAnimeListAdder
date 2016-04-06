692efa37a04f77a895f86159824e82e3555225ea
FoxInFlame <yuto.t@student.intsch.se>
2016-04-06 14:03:35 +0200

    Added Verifying Credentials in Options page

--- options.js
+++ options.js
@@ -6,14 +6,51 @@ function save_options() {
     username: username,
     password: password
   }, function() {
+    $.ajax({
+      url: "http://myanimelist.net/api/account/verify_credentials.xml",
+      type: "GET",
+      dataType: "xml",
+      username: username,
+      password: password,
+      success: function(data) {
+        var status = document.getElementById('status');
+        status.innerHTML = 'Verified Credentials!';
+        status.style.color = "green";
+        chrome.storage.sync.set({
+          verified: true
+        });
+        var verifiedBoxes = document.getElementsByClassName("verified");
+        var verifiedBoxesLength = verifiedBoxes.length;
+        for(var i=0; i < verifiedBoxesLength; i++){
+          verifiedBoxes[i].checked = true;
+        }
+        setTimeout(function() {
+          status.innerHTML = '';
+        }, 3000);
+      },
+    });
     // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.innerHTML = 'Options saved.';
-    setTimeout(function() {
-      status.innerHTML = '';
-    }, 750);
   });
 }
+
+$(document).ajaxError(function(event, jqxhr, settings, exception) {
+  if (jqxhr.status== 401) {
+    var status = document.getElementById("status");
+    status.innerHTML = "Inputted credentials are not valid.";
+    status.style.color = "red";
+    chrome.storage.sync.set({
+      verified: false
+    });
+    var verifiedBoxes = document.getElementsByClassName("verified");
+    var verifiedBoxesLength = verifiedBoxes.length;
+    for(var i=0; i < verifiedBoxesLength; i++){
+      verifiedBoxes[i].checked = false;
+    }
+    setTimeout(function() {
+      status.innerHTML = '';
+    }, 3000);
+  }
+});
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
@@ -21,10 +58,16 @@ function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     username: "Username",
-    password: "password123"
+    password: "password123",
+    verified: false
   }, function(items) {
     document.getElementById("username").value = items.username;
     document.getElementById("password").value = items.password;
+    var verifiedBoxes = document.getElementsByClassName("verified");
+    var verifiedBoxesLength = verifiedBoxes.length;
+    for(var i=0; i < verifiedBoxesLength; i++){
+      verifiedBoxes[i].checked = items.verified;
+    }
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
